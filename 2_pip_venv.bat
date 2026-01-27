7c7301fc96eb1c3fb185ea0252ab9382f62b4bd2
Puustinen Petri <petri.puustinen@etteplan.com>
2026-01-27 10:49:07 +0200

    list pgs example

--- 2_pip_venv.bat
+++ 2_pip_venv.bat
@@ -13,3 +13,6 @@ set pip="%penv%\Scripts\pip.exe"
 :: %pip% uninstall %pkg% && ( pause ) || ( pause )
 :: %pip% install --upgrade %pkg% && ( pause ) || ( pause )
 %pip% install -r "%~dp0\requirements.txt" && ( pause ) || ( pause )
+
+::%pyt% -c "import pkg_resources;pp=[str(p) for p in pkg_resources.working_set];pp.sort();[print(p) for p in pp]" && ( pause ) || ( pause )
+
